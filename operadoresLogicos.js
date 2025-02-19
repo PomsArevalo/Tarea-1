3e52abb170fe8e95deeb56ec3ea55be5888228e8
PomsArevalo <pamelaarevalo12@aragon.unam.mx>
2025-02-19 14:28:14 -0600

    Tarea1

---
+++ operadoresLogicos.js
@@ -0,0 +1,34 @@
+//Tarea 1: Programar el and, el or y el not
+//And
+let and1 = false && false;
+let and2 = false && true;
+let and3 = true && false;
+let and4 = true && true;
+
+console.log("false "+"&&"+" false = "+and1);
+console.log("false "+"&&"+"  true = "+and2);
+console.log(" true "+"&&"+" false = "+and3);
+console.log(" true "+"&&"+"  true = "+and4);
+//Or
+let or1 = false || false;
+let or2 = false || true;
+let or3 = true || false;
+let or4 = true || true;
+
+console.log("false "+"||"+" false = "+or1);
+console.log("false "+"||"+" true  = "+or2);
+console.log(" true "+"||"+" false = "+or3);
+console.log(" true "+"||"+" true  = "+or4);
+
+// Not
+let not1 = !false;
+let not2 = !true;
+
+console.log("!" + "false = " + not1);
+console.log("!" + "true  = " + not2);
+
+
+
+
+
+
